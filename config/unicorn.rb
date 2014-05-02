2836a8d12a99ae9b68349c350bc57a4456ef1879
E Camden Fisher <fish@fishnix.net>
2014-05-01 21:21:50 -0400

    pid files

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,6 +2,6 @@
  worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
  timeout 30
  preload_app true
- pid "tmp/pids/unicorn.pid"
+ pid "tmp/unicorn.pid"
  stderr_path "log/unicorn.stderr.log"
  stdout_path "log/unicorn.stdout.log"
